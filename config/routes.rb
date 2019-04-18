bfecceaeb0ca59c0bcb70c4c32de76c71b78860d
Patrice Drayton <draytonpatrice@marilynssecret.org>
2019-04-18 22:32:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw 
-resources :posts, only: %i[index show new create edit update]
+resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  patch 'post/:id', to: 'posts#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
